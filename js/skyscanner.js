103a6c6957c0ab7e6a11aae7cacfcf3651b4328e
AliXalilov <xalilovgames@gmail.com>
2025-04-05 16:14:16 +0400

    footer added!

--- js/skyscanner.js
+++ js/skyscanner.js
@@ -2,24 +2,32 @@ const quests = document.querySelectorAll(".dw")
 const paper = document.querySelectorAll(".paper")
 const arrow = document.querySelectorAll(".fa-caret-down")
 const lg = document.querySelector(".lg")
+const inter = document.querySelector(".inter")
+const arr_lang = document.querySelector(".arr_lang")
 quests.forEach((quest, index)=>{
     quest.addEventListener("click", ()=>{
         paper[index].classList.toggle("paper_down")
         arrow[index].classList.toggle("fa-caret-up")
     })
 })
-fetch('lang.json')
-      .then(response => response.json())
-      .then(response => console.log(response))
-
-
-lg.forEach((lang, response)=>{
-    const card = document.createElement("div")
-    card.className = "card"
-    card.innerHTML = `
-    <img src = "${response.url}">
-    <a href="#">${response.name}</a>
-    `
-    lg.appendChild(card)
-    console.log("hello")
+inter.addEventListener("click", ()=>{
+    lg.classList.toggle("lg_down")
+    arrow.classList.toggle("fa-caret-up")
 })
+
+fetch('/json/lang.json')
+      .then(response => response.json())
+      .then(dt =>{
+        dt.forEach((lang)=>{
+            const card = document.createElement("div")
+            card.className = "card"
+            card.innerHTML = `
+            <img src = "${lang.url}">
+            <a href="#">${lang.name}</a>
+            `
+            lg.appendChild(card)
+        })
+    })
+
+
+
